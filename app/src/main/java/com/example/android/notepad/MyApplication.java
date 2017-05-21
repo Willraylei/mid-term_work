a57a79c4bee4701109af4b87fdc6bc7a4288b673
AlexLei <leibin@AlexLeis-MacBook-Pro.local>
2017-05-21 14:59:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/notepad/MyApplication.java
@@ -0,0 +1,18 @@
+package com.example.android.notepad;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by leibin on 2017/5/10.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/notepad/NoteAttribute.java
@@ -0,0 +1,14 @@
+package com.example.android.notepad;
+
+/**
+ * Created by leibin on 2017/5/10.
+ */
+public class NoteAttribute {
+
+    public static int snoteBackground;
+
+    public static int scolorselecticon;
+
+    public static final String NOTEBACKGROUND="noteBackground";
+    public static final String COLORSELECTICON="colorselecticon";
+}
